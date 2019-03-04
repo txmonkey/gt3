f6241c509af7a6f5f615674abce5b807a1ca1046
唐侠峰 <>
2019-03-04 16:38:54 +0800

    添加安卓

--- android/src/main/java/mdesigner/ios/microduino/com/gt3/Gt3Plugin.java
+++ android/src/main/java/mdesigner/ios/microduino/com/gt3/Gt3Plugin.java
@@ -1,4 +1,5 @@
-package com.example.jiyan1;
+package mdesigner.ios.microduino.com.gt3;
+
 
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -6,32 +7,263 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** Jiyan1Plugin */
-public class Jiyan1Plugin implements MethodCallHandler {
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-  private static String TAG = Jiyan1Plugin.class.getSimpleName();
-  private static MethodChannel.Result GTresult;
+import com.geetest.sdk.GT3ConfigBean;
+import com.geetest.sdk.GT3ErrorBean;
+import com.geetest.sdk.GT3GeetestUtils;
+import com.geetest.sdk.GT3Listener;
 
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "jiyan1");
-    channel.setMethodCallHandler(new Jiyan1Plugin());
-  }
+import org.json.JSONObject;
 
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else if (call.method.equals("showGeeTest")) {
-      GTresult = result;
-    } else {
-      result.notImplemented();
+
+/**
+ * Gt3Plugin
+ */
+public class Gt3Plugin implements MethodCallHandler {
+
+    private Activity activity;
+
+    private static String TAG = Gt3Plugin.class.getSimpleName();
+
+    /**
+     * api1，需替换成自己的服务器URL
+     */
+    private static final String captchaURL = "http://192.168.50.94/base/captcha";
+    /**
+     * api2，需替换成自己的服务器URL
+     */
+    private static final String validateURL = "http://192.168.50.94/base/captcha";
+    private GT3GeetestUtils gt3GeetestUtils;
+    private GT3ConfigBean gt3ConfigBean;
+    private String resultStr;
+
+
+    private static MethodChannel.Result GTresult;
+
+    /**
+     * Plugin registration.
+     */
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "gt3");
+        channel.setMethodCallHandler(new Gt3Plugin(registrar.activity()));
     }
-  }
 
-  public static void GTResult(String status) {
-    GTresult.success(status);
-  }
+    public Gt3Plugin(Activity activity) {
+        this.activity = activity;
+        gt3GeetestUtils = new GT3GeetestUtils(this.activity);
+    }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        if (call.method.equals("getPlatformVersion")) {
+            result.success("Android " + android.os.Build.VERSION.RELEASE);
+        } else if (call.method.equals("showGeeTest")) {
+            GTresult = result;
+            customVerity();
+        } else {
+            result.notImplemented();
+        }
+    }
+
+    public static void GTResult(String status) {
+        GTresult.success(status);
+    }
+
+
+    private void customVerity() {
+        // 配置bean文件，也可在oncreate初始化
+        gt3ConfigBean = new GT3ConfigBean();
+        // 设置验证模式，1：bind，2：unbind
+        gt3ConfigBean.setPattern(1);
+        // 设置点击灰色区域是否消失，默认不消失
+        gt3ConfigBean.setCanceledOnTouchOutside(false);
+        // 设置debug模式，开代理可调试 TODO 线上版本关闭
+        gt3ConfigBean.setDebug(false);
+        // 设置语言，如果为null则使用系统默认语言
+        gt3ConfigBean.setLang(null);
+        // 设置加载webview超时时间，单位毫秒，默认10000，仅且webview加载静态文件超时，不包括之前的http请求
+        gt3ConfigBean.setTimeout(10000);
+        // 设置webview请求超时(用户点选或滑动完成，前端请求后端接口)，单位毫秒，默认10000
+        gt3ConfigBean.setWebviewTimeout(10000);
+        // 设置回调监听
+        gt3ConfigBean.setListener(new GT3Listener() {
+
+            /**
+             * api1结果回调
+             * @param result
+             */
+            @Override
+            public void onApi1Result(String result) {
+                Log.e(TAG, "GT3BaseListener-->onApi1Result-->" + result);
+            }
+
+            /**
+             * 验证码加载完成
+             * @param duration 加载时间和版本等信息，为json格式
+             */
+            @Override
+            public void onDialogReady(String duration) {
+                Log.e(TAG, "GT3BaseListener-->onDialogReady-->" + duration);
+            }
+
+            /**
+             * 验证结果
+             * @param result
+             */
+            @Override
+            public void onDialogResult(String result) {
+                Log.e(TAG, "GT3BaseListener-->onDialogResult-->" + result);
+                // 开启api2逻辑
+                if (!TextUtils.isEmpty(result)) {
+                    try {
+                        JSONObject jsonObject = new JSONObject(result);
+                        Log.e(TAG, "GT3BaseListener-->onDialogResult-->" + jsonObject);
+                        String geetest_challenge = jsonObject.getString("geetest_challenge");
+                        String geetest_seccode = jsonObject.getString("geetest_seccode");
+                        String geetest_validate = jsonObject.getString("geetest_validate");
+                        resultStr = "geetest_challenge=" + geetest_challenge + "&geetest_seccode=" + geetest_seccode + "&geetest_validate" + geetest_validate;
+                    } catch (Exception e) {
+
+                    }
+                }
+
+                new Gt3Plugin.RequestAPI2().execute(result);
+            }
+
+            /**
+             * api2回调
+             * @param result
+             */
+            @Override
+            public void onApi2Result(String result) {
+                Log.e(TAG, "GT3BaseListener-->onApi2Result-->" + result);
+            }
+
+            /**
+             * 统计信息，参考接入文档
+             * @param result
+             */
+            @Override
+            public void onStatistics(String result) {
+                Log.e(TAG, "GT3BaseListener-->onStatistics-->" + result);
+            }
+
+            /**
+             * 验证码被关闭
+             * @param num 1 点击验证码的关闭按钮来关闭验证码, 2 点击屏幕关闭验证码, 3 点击返回键关闭验证码
+             */
+            @Override
+            public void onClosed(int num) {
+                Log.e(TAG, "GT3BaseListener-->onClosed-->" + num);
+            }
+
+            /**
+             * 验证成功回调
+             * @param result
+             */
+            @Override
+            public void onSuccess(String result) {
+                Log.e(TAG, "GT3BaseListener-->onSuccess-->" + result);
+            }
+
+            /**
+             * 验证失败回调
+             * @param errorBean 版本号，错误码，错误描述等信息
+             */
+            @Override
+            public void onFailed(GT3ErrorBean errorBean) {
+                Log.e(TAG, "GT3BaseListener-->onFailed-->" + errorBean.toString());
+            }
+
+            /**
+             * api1回调
+             */
+            @Override
+            public void onButtonClick() {
+                new Gt3Plugin.RequestAPI1().execute();
+            }
+        });
+        gt3GeetestUtils.init(gt3ConfigBean);
+        // 开启验证
+        gt3GeetestUtils.startCustomFlow();
+    }
+
+    /**
+     * 请求api1
+     */
+    class RequestAPI1 extends AsyncTask<Void, Void, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(Void... params) {
+            String string = HttpUtils.requestGet(captchaURL);
+            Log.e(TAG, "doInBackground: " + string);
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = new JSONObject(string);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return jsonObject;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject parmas) {
+            // 继续验证
+            Log.i(TAG, "RequestAPI1-->onPostExecute: " + parmas);
+            // SDK可识别格式为
+            // {"success":1,"challenge":"06fbb267def3c3c9530d62aa2d56d018","gt":"019924a82c70bb123aae90d483087f94","new_captcha":true}
+            // TODO 设置返回api1数据，即使为null也要设置，SDK内部已处理
+            gt3ConfigBean.setApi1Json(parmas);
+            // 继续api验证
+            gt3GeetestUtils.getGeetest();
+        }
+    }
+
+    /**
+     * 请求api2
+     */
+    class RequestAPI2 extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            if (!TextUtils.isEmpty(params[0])) {
+                return HttpUtils.requestPost(validateURL, params[0]);
+            } else {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.i(TAG, "RequestAPI2-->onPostExecute: " + result);
+            if (!TextUtils.isEmpty(result)) {
+                try {
+                    JSONObject jsonObject = new JSONObject(result);
+                    String status = jsonObject.getString("errorCode");
+                    if ("200".equals(status)) {
+                        GTresult.success(resultStr);
+                        gt3GeetestUtils.showSuccessDialog();
+                    } else {
+                        gt3GeetestUtils.showFailedDialog();
+                    }
+                } catch (Exception e) {
+                    gt3GeetestUtils.showFailedDialog();
+                    e.printStackTrace();
+                }
+            } else {
+                gt3GeetestUtils.showFailedDialog();
+            }
+        }
+    }
 
 
 }
